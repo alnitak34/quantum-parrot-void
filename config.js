4a251d47b7c583506f1456cf38584873177cdaee
alnitak34 <alnitak34@users.noreply.github.com>
2026-05-07 06:43:45 +0200

    fix supabase url config

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 
 
-window.SUPABASE_URL = "https://fdjdwfdmqqyzkvqwkelk.supabase.co/rest/v1/";
+window.SUPABASE_URL = "https://fdjdwfdmqqyzkvqwkelk.supabase.co";
 window.SUPABASE_ANON_KEY = "sb_publishable_cwmeuLKWxTcDon9vofJ0xQ_hu67qQvc";
